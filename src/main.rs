fe654114dc7d8dc18bda52279aa03bfe6b071c35
jkarrer <jkarrer@protect247.com>
2023-03-06 22:28:15 -0500

    :broom: Clean up comments and whitespace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-
-// A command line app that will store my snippets, gists, and links into a repo.
-// This will
-// - Take an argument for the type of thing to store
-// - Take an argument for the item, file, or text
-// - Take an argument of some type of organization or group
-
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -13,7 +6,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let link = &args[1];
     let tag = &args[2];
-    dbg!(link, tag);
 
     let formatted_link = format!("[{}]({})\n", tag, link);
 
