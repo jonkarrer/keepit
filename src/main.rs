04bfb5aca9d69b11d20006b172afd67fa8039b79
jkarrer <jkarrer@protect247.com>
2023-03-16 15:18:41 -0400

    :fire: remove git commands

--- src/main.rs
+++ src/main.rs
@@ -24,27 +24,5 @@ fn main() {
 
     // Write the cli arguments to the file
     file.write_all(readme_formatted_link.as_bytes()).expect("Failed to write to file");
-
-    // Run git commands to commit to a repo.
-    Command::new("git")
-        .arg("add")
-        .arg(".")
-        .status()
-        .expect("Failed git add");
-    
-    Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg("added new link")
-        .status()
-        .expect("Failed to commit");
-    
-    Command::new("git")
-        .arg("push")
-        .arg("-u")
-        .arg("origin")
-        .arg("main")
-        .status()
-        .expect("Failed to push");
 }
 
